19670821f6a5e37db74cf4583eecd45038cb67a9
Rob Aiken <rob@aiken.im>
2020-01-25 17:01:18 +0000

    socketcontroller

---
+++ Assets/SocketData/GameState.cs
@@ -0,0 +1,9 @@
+namespace SocketData
+{
+    public class GameState
+    {
+        public float TotalEnergy;
+        public bool HasGameComplete = false;
+        public VillagerSpawner[] VillagerSpawners;
+    }
+}
