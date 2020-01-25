3d4b6f679080e58ba16c78333b5bed5c6a8c2992
dsavosin95 <ingamestech@gmail.com>
2020-01-25 12:57:00 +0000

    Added basic ground units logic. Fist smash mechanic and scare mechanic.

---
+++ Assets/GroundUnit.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundUnit : MonoBehaviour
+{
+    [SerializeField]
+    float explosionForce;
+
+    [SerializeField]
+    float explosionRadius;
+
+    [SerializeField]
+    float explosionLift;
+
+    [SerializeField]
+    float rotationSpeed;
+
+    [SerializeField]
+    float attackRadius;
+
+    [SerializeField]
+    float unitSpeed;
+
+    public Transform[] soldiers;
+
+    Transform playerTarget;
+    Transform gazeTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Fill in array of soldiers
+        soldiers = GetComponentsInChildren<Transform>();
+
+        playerTarget = Camera.main.transform;
+
+        if(GameObject.FindObjectOfType<GazePoint>() != null)
+        {
+            gazeTarget = GameObject.FindObjectOfType<GazePoint>().transform;
+        }
+
+        // Default unit rotation to start looking at the player despite its spawned rotation
+        Vector3 playerFloorPos = new Vector3(playerTarget.position.x, 0.0f, playerTarget.position.z);
+        Quaternion quarternion = Quaternion.LookRotation(playerFloorPos - transform.position);
+        transform.rotation = quarternion;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            SmackEm();
+            StartCoroutine(DestroyUnit(4.0f));
+        }
+
+        Vector3 playerFloorPos = new Vector3(playerTarget.position.x, 0.0f, playerTarget.position.z);
+        Quaternion quarternion = Quaternion.LookRotation(playerFloorPos - transform.position);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, quarternion, rotationSpeed * Time.deltaTime);
+
+        //If distance between this unit and a player is more than attackRadius, run towars it.
+        if(Vector3.Distance(playerFloorPos, transform.position) > attackRadius)
+        {
+            float step = unitSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, playerFloorPos, step);
+        }
+    }
+
+
+    public void SmackEm()
+    {
+        foreach(Transform soldier in soldiers)
+        {
+            Rigidbody rb = soldier.GetComponent<Rigidbody>();
+
+            if(rb != null)
+            {
+                rb.isKinematic = false;
+                rb.useGravity = true;
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius, explosionLift);
+
+                //Disable animation controllers per entity
+            }
+        }
+    }
+
+    public void ScareEm()
+    {
+        foreach (Transform soldier in soldiers)
+        {
+            Rigidbody rb = soldier.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.isKinematic = false;
+                rb.useGravity = false;
+
+                Vector3 randDir = new Vector3(Random.Range(-1.0f, 1.0f), 0.0f, Random.Range(-1.0f, 1.0f));
+                rb.velocity = randDir;
+
+                //Disable animation controllers per entity
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<LimbEnergyController>())
+        {
+            Debug.Log("Ground Unit collided with a hand!");
+            SmackEm();
+            StartCoroutine(DestroyUnit(4.0f));
+        }
+
+        if (other.gameObject.CompareTag("GazePoint"))
+        {
+            Debug.Log("Giant looks at this unit");
+            ScareEm();
+            StartCoroutine(ReturnToUnit(4.0f));
+        }
+    }
+
+    IEnumerator ReturnToUnit(float delay = 0.0f)
+    {
+        if (delay != 0)
+            yield return new WaitForSeconds(delay);
+
+        // The rest of your coroutine here
+        foreach (Transform soldier in soldiers)
+        {
+            Rigidbody rb = soldier.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.velocity = Vector3.zero;
+            }
+
+            SoldierUnit singleUnit = soldier.GetComponent<SoldierUnit>();
+            if(singleUnit != null)
+            {
+                singleUnit.returnToPosition = true;
+            }
+        }
+    }
+
+    IEnumerator DestroyUnit(float delay = 0.0f)
+    {
+        if (delay != 0)
+            yield return new WaitForSeconds(delay);
+
+        // The rest of your coroutine here
+        foreach (Transform soldier in soldiers)
+        {
+            Destroy(soldier.gameObject);
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/SoldierUnit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierUnit : MonoBehaviour
+{
+    [SerializeField]
+    float soldierSpeed;
+
+    public Vector3 localInitPosition;
+    public Quaternion localInitRotation;
+    public bool returnToPosition;
+    
+    void Start()
+    {
+        returnToPosition = false;
+        localInitPosition = transform.localPosition;
+        localInitRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        if(returnToPosition)
+        {
+            returnToPosition = false;
+            transform.localPosition = localInitPosition;
+            transform.rotation = localInitRotation;
+        }
+    }
+}
