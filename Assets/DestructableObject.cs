125c73d49a560fe197f6f967a57289c2e07c69ad
dsavosin95 <ingamestech@gmail.com>
2020-01-26 01:03:03 +0000

    Fixed ground units damage, added spawning, finished LD.

--- Assets/DestructableObject.cs
+++ Assets/DestructableObject.cs
@@ -11,24 +11,34 @@ public class DestructableObject : MonoBehaviour
     [SerializeField]
     float damageReceive;
 
+
     public Transform hitFX;
 
+    float waitForSeconds;
 
     public float DestructionThreshold = 1;
     // Start is called before the first frame update
     void Start()
     {
+        waitForSeconds = 3.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        waitForSeconds += Time.deltaTime;
+        if(waitForSeconds >= 3.0f)
+        {
+            waitForSeconds = 3.0f;
+        }
+
+
         if (Input.GetKeyDown(KeyCode.P))
         {
             iTween.PunchPosition(gameObject, iTween.Hash("amount", new Vector3(0.5f, 0.5f, 0.5f), "time", 1, "delay", 0.1, "loopType", "none"));
 
-            if(hitFX != null)
-                hitFX.GetComponentInChildren<ParticleSystem>().Play();
+            //if(hitFX != null)
+                //hitFX.GetComponentInChildren<ParticleSystem>().Play();
         }
     }
 
@@ -39,17 +49,19 @@ public class DestructableObject : MonoBehaviour
 
         if (collision.gameObject.CompareTag("LeftHand"))
         {
-            if (EnergyController.instance.leftHandVelo > DestructionThreshold)
+            if (EnergyController.instance.leftHandVelo > DestructionThreshold && waitForSeconds >= 3.0f)
             {
                 dealDamage = true;
+                waitForSeconds = 0.0f;
             }
         }
 
         if (collision.gameObject.CompareTag("RightHand"))
         {
-            if (EnergyController.instance.rightHandVelo > DestructionThreshold)
+            if (EnergyController.instance.rightHandVelo > DestructionThreshold && waitForSeconds >= 3.0f)
             {
                 dealDamage = true;
+                waitForSeconds = 0.0f;
             }
         }
 
@@ -77,12 +89,19 @@ public class DestructableObject : MonoBehaviour
         {
             health -= damageReceive;
 
-            if (hitFX != null)
-                hitFX.GetComponentInChildren<ParticleSystem>().Play();
+            if(EnergyController.instance.hitCastleFirstTime == false)
+            {
+                EnergyController.instance.SpawnUnit();
+                EnergyController.instance.hitCastleFirstTime = true;
+                //EnergyController.instance.canSpawn = true;
+            }
 
             iTween.PunchPosition(gameObject, iTween.Hash("amount", new Vector3(0.5f, 0.5f, 0.5f), "time", 1, "delay", 0.1, "loopType", "none"));
             if (health <= 0.0f)
             {
+                if (hitFX != null)
+                    hitFX.GetComponentInChildren<ParticleSystem>().Play();
+
                 StartCoroutine(DestroyObject(1.5f));
             }
         }
--- Assets/EnergyController.cs
+++ Assets/EnergyController.cs
@@ -9,18 +9,55 @@ public class EnergyController : MonoBehaviour
     public static EnergyController instance;
 
     public float leftHandVelo, rightHandVelo;
+
+    public GameObject groundUnitPrefab;
+
+    public Transform spawnPoint1, spawnPoint2;
+
+    bool flipSwitch;
+
+    public bool hitCastleFirstTime;
+
+
+    public bool canSpawn;
+
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+        flipSwitch = false;
+        hitCastleFirstTime = false;
+        canSpawn = false;
     }
-
 
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            SpawnUnit();
+        }
+
+        if(energy > 50 && canSpawn)
+        {
+            canSpawn = false;
+            SpawnUnit();
+        }
+    }
+
+    public void SpawnUnit()
+    {
+        if (flipSwitch)
+        {
+            Instantiate(groundUnitPrefab, spawnPoint1.position, spawnPoint1.rotation);
+            flipSwitch = false;
+        }
+        else
+        {
+            Instantiate(groundUnitPrefab, spawnPoint2.position, spawnPoint2.rotation);
+            flipSwitch = true;
+        }
     }
 
     public void AddSetEnergy(float val)
@@ -55,6 +92,11 @@ public class EnergyController : MonoBehaviour
         if (energy >= 0 && energy < 1000)
         {
             energy -= velo;
+        }
+
+        if(energy <= 50 && canSpawn == false)
+        {
+            canSpawn = true;
         }
     }
 }
